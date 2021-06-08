7e42d9dd346b8c8b9543103369368dfdf4b7c9aa
champaksworld <champaksworld@users.noreply.github.com>
2021-06-08 11:29:01 +0530

    Named route without home

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,14 @@ import 'vsjone.dart';
 import 'vsjtwo.dart';
 
 void main() {
-  runApp(VSJApp());
+   MaterialApp(
+
+    initialRoute: '/',
+    routes: {
+        '/': (context) => VsjOne(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => VsjTwo(),
+    },
+  );
 }
 
-class VSJApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: VsjOne(),
-      //initialRoute: '/',
-      routes: {
-        //  '/': (context) => VsjOne(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => VsjTwo(),
-      },
-    );
-  }
-}
