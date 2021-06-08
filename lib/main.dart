31b763ee874d4cf546d60de94f70bed1fcef4f64
champaksworld <champaksworld@users.noreply.github.com>
2021-06-08 11:24:05 +0530

    Named Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'vsjone.dart';
 import 'vsjtwo.dart';
+
 void main() {
   runApp(VSJApp());
 }
 
 class VSJApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'VSJ Navigation Demo',
-
       home: VsjOne(),
+      //initialRoute: '/',
+      routes: {
+        //  '/': (context) => VsjOne(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => VsjTwo(),
+      },
     );
   }
 }
--- lib/vsjone.dart
+++ lib/vsjone.dart
@@ -5,21 +5,20 @@ class VsjOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
         title: Text('VSJ One'),
       ),
       body: Center(
-        backgroundColor: Colors.teal,
         child: ElevatedButton(
           child: Text('Go to Two'),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => VsjTwo()),
-            );
-          },
+    Navigator.pushNamed(context, '/second');
+    }
+            ),
+
         ),
-      ),
-    );
+      );
+
   }
 }
